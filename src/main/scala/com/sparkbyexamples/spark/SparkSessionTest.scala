7efebc86fa2081977c4f30aa67b7a9596b9e014b
AIMMY-CAMTPC\noppo <noppon.w@cmu.ac.th>
2022-08-29 10:22:00 +0700

    Test SparkSession.

--- src/main/scala/com/sparkbyexamples/spark/SparkSessionTest.scala
+++ src/main/scala/com/sparkbyexamples/spark/SparkSessionTest.scala
@@ -8,7 +8,7 @@ object SparkSessionTest {
 
 
     val spark = SparkSession.builder()
-      .master("local[*]")
+      //.master("local[*]")
       .appName("SparkByExample")
       .getOrCreate();
     
